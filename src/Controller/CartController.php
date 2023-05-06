225760b0a688dcbf0c96c4cfc406e34735969cba
Affi-Amine <affi.amin.work@gmail.com>
2023-05-06 22:54:19 +0100

    beginning to work on my new branch -amine

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -5,24 +5,22 @@ namespace App\Controller;
 use App\Repository\AccommodationRepository;
 use App\Repository\ActivityRepository;
 use App\Repository\FoodRepository;
+use App\Service\ReservationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
+
+    public function __construct(private ReservationService $reservationService)
+    {
+    }
+
     #[Route('/cart', name: 'app_cart')]
     public function index(AccommodationRepository $accommodationRepository, FoodRepository $foodRepository
         , ActivityRepository  $activityRepository): Response
     {
-        $accommodationCards = $accommodationRepository->findAll();
-        $foodCards = $foodRepository->findAll();
-        $activityCards = $activityRepository->findAll();
 
-        return $this->render('cart/index.html.twig', [
-            'accommodationCards' => $accommodationCards,
-            'foodCards' => $foodCards,
-            'activityCards' => $activityCards
-        ]);
     }
 }
