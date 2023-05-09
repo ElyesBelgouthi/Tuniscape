0c0f7ff93cdbaced9931d5546c8bdea384b8eca3
Omar-Maalej <omar.maalej@insat.ucar.tn>
2023-05-09 19:58:39 +0100

    Added Catalog with pagination - Added Admin Space - Added Image Upload (error in fileinfo module)

--- src/Controller/AccommodationController.php
+++ src/Controller/AccommodationController.php
@@ -6,14 +6,17 @@ use App\Entity\Accommodation;
 use App\Form\AccommodationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AccommodationController extends AbstractController
-{   #[Route('/accommodation', 'accommodation_list_all')]
+{   #[Route('/accommodation/', 'accommodation_list_all')]
     public function index(ManagerRegistry $doctrine) : Response{
         $repository = $doctrine->getRepository(Accommodation::class);
         $accommodations = $repository->findAll();
@@ -23,7 +26,7 @@ class AccommodationController extends AbstractController
 
     }
     #[Route('/accommodation/edit/{id?0}', name: 'app_accommodation_edit')]
-    public function addAccommodation(Request $request, EntityManagerInterface $entityManager, Accommodation $accommodation = null): Response
+    public function addAccommodation(Request $request, EntityManagerInterface $entityManager,SluggerInterface $slugger, Accommodation $accommodation = null): Response
     {   $isAdded = false;
 
         if(!$accommodation){
@@ -34,7 +37,30 @@ class AccommodationController extends AbstractController
         $form = $this->createForm(AccommodationType::class, $accommodation);
         $form->handleRequest($request);
 
-        if($form->isSubmitted()){
+        if($form->isSubmitted()&&$form->isValid()){
+            /** @var UploadedFile $photo */
+            $photo = $form->get('photo')->getData();
+            if ($photo) {
+                $originalFilename = pathinfo($photo->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$photo->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $photo->move(
+                        $this->getParameter('accommodation_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $accommodation->setImage($newFilename);
+            }
+
             $entityManager->persist($accommodation);
             $entityManager->flush();
             if($isAdded){
