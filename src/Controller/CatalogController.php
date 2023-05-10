da99fd8c25038e5335fcc367efc4c396a5bc441b
Omar-Maalej <omar.maalej@insat.ucar.tn>
2023-05-10 13:54:04 +0100

    Added a filter for accommodation and activities in catalog - needs some small fixes

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -5,58 +5,81 @@ namespace App\Controller;
 use App\Entity\Accommodation;
 use App\Entity\Activity;
 use App\Entity\Food;
+use App\Form\RegionFormType;
+use App\Form\RegionType;
 use App\Repository\AccommodationRepository;
+use App\Repository\RegionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CatalogController extends AbstractController
 {
     #[Route('/catalog/{entityName}/{page?1}', name: 'app_catalog')]
-    public function index(EntityManagerInterface $entityManager,string $entityName = null, int $page ): Response
-    {   $nb = 8;
-        $cards=null;
-        $repository=null;
+    public function index(Request $request, RegionRepository $regionRepository, EntityManagerInterface $entityManager, string $entityName = null, int $page): Response
+    {
+        $region = null;
+        $nb = 8;
+        $cards = null;
+        $repository = null;
         $size = null;
         $folderName = null;
-        switch($entityName){
+
+        switch ($entityName) {
             case 'accommodation':
                 $repository = $entityManager->getRepository(Accommodation::class);
-                $cards=$repository->findBy([],[],$nb,($page-1)*$nb);
-                $size = sizeof($repository->findAll());
-                $folderName = "uploads/accommodations/";
-
-
                 break;
             case 'food':
                 $repository = $entityManager->getRepository(Food::class);
-                $cards=$repository->findBy([],[],$nb,($page-1)*$nb);
-                $size = sizeof($repository->findAll());
-                $folderName = "uploads/foods/";
-
                 break;
             case 'activity':
                 $repository = $entityManager->getRepository(Activity::class);
-                $cards=$repository->findBy([],[],$nb,($page-1)*$nb);
-                $size = sizeof($repository->findAll());
-                $folderName = "uploads/activities/";
-
                 break;
-
-
+            default:
+                return $this->redirectToRoute("app_home");
         }
 
+        if ($entityName === 'accommodation' || $entityName === 'activity') {
+            $regionForm = $this->createForm(RegionFormType::class, $region);
+            $regionForm->handleRequest($request);
 
-        if($entityName == 'accommodation' or $entityName == 'food' or $entityName == 'activity'){
+            if ($regionForm->isSubmitted() && $regionForm->isValid()) {
+                $region = $regionForm->get('region')->getData();
+            }
+        }
+
+        if ($region) {
+            $cards = $repository->findBy(['region' => $region], [], $nb, ($page - 1) * $nb);
+            $size = sizeof($repository->findBy(['region' => $region]));
+        } else {
+            $cards = $repository->findBy([], [], $nb, ($page - 1) * $nb);
+            $size = sizeof($repository->findAll());
+        }
+
+        switch ($entityName) {
+            case 'accommodation':
+                $folderName = "uploads/accommodations/";
+                break;
+            case 'food':
+                $folderName = "uploads/foods/";
+                break;
+            case 'activity':
+                $folderName = "uploads/activities/";
+                break;
+        }
+
         return $this->render('catalog/index.html.twig', [
-            'cards'=>$cards,
-            'page'=>$page,
-            'name'=>$entityName,
-            'max'=> ceil($size / $nb),
-            'folderName'=> $folderName
-        ]);} else {
-            return $this->redirectToRoute("app_home");
-        }
+            'cards' => $cards,
+            'page' => $page,
+            'name' => $entityName,
+            'max' => ceil($size / $nb),
+            'folderName' => $folderName,
+            'regionForm' => ($entityName === 'accommodation' || $entityName === 'activity') ? $regionForm : null,
+        ]);
     }
+
+
+
 }
